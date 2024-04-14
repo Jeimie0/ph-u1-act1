04549b23a81cb243f6c3a600972cb8a9ceed47b0
Jeimie0 <102849361+Jeimie0@users.noreply.github.com>
2024-04-13 23:07:23 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.u1.ac1',
+  appName: 'ph-u1-ac1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
